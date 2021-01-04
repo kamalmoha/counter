201cab07f2e8814059ae9860a87ff41feffcf39c
Kamal Mohamed <kamaalkowdiid@gmail.com>
2021-01-04 13:56:41 +0300

    first cmmit

---
+++ setup/app.js
@@ -0,0 +1,29 @@
+let count =0;
+
+const value= document.querySelector("#value");
+const btns= document.querySelectorAll(".btn")
+
+btns.forEach( function (btn){
+btn.addEventListener("click", function(e){
+    const abdi= e.currentTarget.classList;
+    if (abdi.contains("decrease")) {
+        count--;
+    } else if (abdi.contains("increase")) {
+        count++;
+    } else {
+        count = 0;
+    } 
+
+    if (count > 0) {
+        value.style.color = "green";
+      }
+      if (count < 0) {
+        value.style.color = "red";
+      }
+      if (count === 0) {
+        value.style.color = "#purple";
+      }
+      value.textContent = count;
+    
+});
+});
